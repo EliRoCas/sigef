91a7d62138c322f8ac2c781c362d1e3fa334853b
Eli Rosero <elir_94@hotmail.com>
2023-12-11 10:46:40 -0500

    header-nav-footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule} from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './structure/header/header.component';
+import { NavComponent } from './structure/header/nav/nav.component';
+import { FooterComponent } from './structure/footer/footer.component';
+import { HomeComponent } from './modules/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
